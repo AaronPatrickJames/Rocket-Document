1ceb76936ca50ca0b56b073e30c3699d654925ea
Aaron James <36738943+AaronPatrickJames@users.noreply.github.com>
2024-02-11 17:42:20 -0500

    Add mongo  db connection class

---
+++ Rocket Document/ClassMongoDBConnection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class MongoDatabaseConnection
+{
+	public string MongoPrefix;
+	public string MongoUserName;
+	public string MongoPassword;
+	public string MongoCluster;
+	public string MongoPostfix;
+
+	public Class1()
+	{
+	}
+}
